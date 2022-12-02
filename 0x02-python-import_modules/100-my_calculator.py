fff132ab2efff30de270c0dd6040b6b430727931
kenny-Hassan <atanda.m1703773@st.futminna.edu.ng>
2022-12-02 02:40:10 +0000

    task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+    import sys
+    from calculator_1 import add, sub, div, mul
+
+    if len(sys.argv) -1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    oprand = {"+": add, "-": sub, "/": div, "*": mul}
+    if sys.argv[2] not in list(oprand.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
